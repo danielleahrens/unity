cab774467d58f271d350a169c4a880ed97ba7ce0
Danielle Ahrens <danielle.c.ahrens@gmail.com>
2021-10-03 15:38:04 -0400

    simpler (and better looking) page design. Only copy that is needed at this time

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import './App.css';
-import About from './About.js'
-import Ceremony from './Ceremony.js'
-import Reception from './Reception.js'
-import OutOfTowners from './OutOfTowners.js'
-import Registry from './Registry.js'
-import Rsvp from './Rsvp.js'
-import Faq from './Faq.js'
+import Home from './Home.js'
 
 class App extends React.Component {
 
@@ -16,7 +10,7 @@ class App extends React.Component {
 
   componentDidMount() {
     this.setState({
-      display: 'About'
+      display: 'Home'
     })
   }
 
@@ -29,24 +23,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="header">
-          <div className="header-menu">
-            <a className={(this.state.display === 'About') ? 'header-menu-item on-focus' : 'header-menu-item'} onClick={() => this.setState({display: 'About'})}>About Us</a>
-            {/* <a className="header-menu-item" onClick={() => this.setState({display: 'Ceremony'})}>Ceremony</a> */}
-            <a className={(this.state.display === 'Reception') ? 'header-menu-item on-focus' : 'header-menu-item'} onClick={() => this.setState({display: 'Reception'})}>Reception</a>
-            <a className={(this.state.display === 'OutOfTowners') ? 'header-menu-item on-focus' : 'header-menu-item'} onClick={() => this.setState({display: 'OutOfTowners'})}>Things To Do</a>
-            <a className={(this.state.display === 'Registry') ? 'header-menu-item on-focus' : 'header-menu-item'} onClick={() => this.setState({display: 'Registry'})}>Registry</a>
-            {<a className={(this.state.display === 'Faq') ? 'header-menu-item on-focus' : 'header-menu-item'} onClick={() => this.setState({display: 'Faq'})}>FAQ</a>}
-          </div>
-        </header>
         <div className="body">
-          {(this.state.display === 'About') ? <About /> : <div /> }
-          {/* {(this.state.display === 'Ceremony') ? <Ceremony ceremonyCallback = {this.callbackFunction.bind(this)}/> : <div /> } */}
-          {(this.state.display === 'Reception') ? <Reception receptionCallback = {this.callbackFunction.bind(this)}/> : <div /> }
-          {(this.state.display === 'OutOfTowners') ? <OutOfTowners /> : <div /> }
-          {(this.state.display === 'Registry') ? <Registry /> : <div /> }
-          {(this.state.display === 'Rsvp') ? <Rsvp eventType = {this.state.display}/> : <div /> }
-          {(this.state.display === 'Faq') ? <Faq /> : <div /> }
+          {(this.state.display === 'Home') ? <Home /> : <div /> }
         </div>
       </div>
     );
