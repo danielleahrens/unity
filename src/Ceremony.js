cdb64b0a34730614a86b3e085e53bb67611979f6
Danielle Ahrens <danielle.c.ahrens@gmail.com>
2022-03-19 21:22:16 -0400

    add routing to separate out events

---
+++ src/Ceremony.js
@@ -0,0 +1,13 @@
+import './Ceremony.css';
+
+function Ceremony() {
+    return(
+        <div className="Ceremony">
+            <h1>🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧</h1>
+            <h2>Under Construction</h2>
+            <h2>Come Back Later</h2>
+        </div>
+    )
+}
+
+export default Ceremony
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Home from './Home.js';
+import CeremonyApp from './CeremonyApp.js'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootElement = document.getElementById("root");
+render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="wedding" element={<App />} />
+            <Route path="ceremony" element={<CeremonyApp />} />
+        </Routes>
+    </BrowserRouter>, 
+    rootElement
 );
