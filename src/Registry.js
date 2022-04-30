eafbe66138970e6acca79a73808639cfa7a8000d
Danielle Ahrens <danielle.c.ahrens@gmail.com>
2022-04-30 16:47:56 -0400

    text-align left instead of justify

--- src/Registry.js
+++ src/Registry.js
@@ -4,13 +4,13 @@ function Registry() {
     return(
         <div className="Registry">
             <h1>Registry</h1>
-            <p>Izaak and I have had 11 years together where we've slowly built up our possessions. We are also privileged to be in a place where most things that we need or want, we can go buy without a much thought. For that reason, in lieu of a traditional registry, we're asking folks to donate either to a non-profit of your choice or to one of the local non-profits we support in the U.P. - the Keweenaw Land Trust.</p>
+            <p className="registry-text">Izaak and I have had 11 years together where we've slowly built up our possessions. We are also privileged to be in a place where most things that we need or want, we can go buy without a much thought. For that reason, in lieu of a traditional registry, we're asking folks to donate either to a non-profit of your choice or to one of the local non-profits we support in the U.P. - the Keweenaw Land Trust.</p>
             <h2>Keweenaw Land Trust</h2>
-            <p>The Keweenaw Land Trust is a community partner protecting land, water, and quality of life through conservation, stewardship and education in the Keweenaw. They maintain a number of trails in the area that we enjoy hiking and do great work conserving the beauty of our neck of the woods.</p> 
-            <p>Izaak started volunteering with the Land Trust in early 2021, providing them with IT support as they upgraded their systems to the cloud and has been active in supporting them ever since. We would love it if you'd consider making a donation by going to <a href="https://keweenawlandtrust.org" target="_blank">keweenawlandtrust.org</a></p>
+            <p className="registry-text">The Keweenaw Land Trust is a community partner protecting land, water, and quality of life through conservation, stewardship and education in the Keweenaw. They maintain a number of trails in the area that we enjoy hiking and do great work conserving the beauty of our neck of the woods.</p> 
+            <p className="registry-text">Izaak started volunteering with the Land Trust in early 2021, providing them with IT support as they upgraded their systems to the cloud and has been active in supporting them ever since. We would love it if you'd consider making a donation by going to <a href="https://keweenawlandtrust.org" target="_blank">keweenawlandtrust.org</a></p>
             <h2>But what if I really want to give you something?</h2>
-            <p>We understand some folks really want to bring a gift with them. In this case, the things we're most lacking in is outdoor/lawn care things. Before going out and buying anything, we'd ask that you look to see if you have extra items at your house that you don't have a need for that we could take off your hands.</p>
-            <p>Some examples of things we don't have, but could use:</p>
+            <p className="registry-text">We understand some folks really want to bring a gift with them. In this case, the things we're most lacking in is outdoor/lawn care things. Before going out and buying anything, we'd ask that you look to see if you have extra items at your house that you don't have a need for that we could take off your hands.</p>
+            <p className="registry-text">Some examples of things we don't have, but could use:</p>
             <div className="registry-list">
                 <div className="registry-list-group">
                     <p className="registry-list-header">Garden Supplies</p>
@@ -46,7 +46,7 @@ function Registry() {
                 </div>
             </div>
             <div className="registry-list-full">
-                <p className="registry-list-header">We are still trying to fill out our house with some furniture. If you're in a place in your life where you're looking to downsize your possessions, we should talk! Here are some furniture things we're on the hunt for now:</p>
+                <p className="registry-list-header-full">We are still trying to fill out our house with some furniture. If you're in a place in your life where you're looking to downsize your possessions, we should talk! Here are some furniture things we're on the hunt for now:</p>
                 <li>Bedside tables</li>
                 <li>Small desk</li>
                 <li>Bookshelves</li>
